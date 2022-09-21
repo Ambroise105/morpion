238439639ae7aa6f971903354ad893cc7fd3ed73
Alex <alex.ardiot@gmail.com>
2022-09-21 16:32:31 +0200

    Presque fini

--- morpion.py
+++ morpion.py
@@ -1,27 +1,91 @@
-liste_coup_joueur1 = ["A1", "A2", "B1"]
+#Fonction qui vérifie si le joueur 1 a gagné
+from os import remove
 
-if "A1" and "A2" and "A3" in liste_coup_joueur1:      #A1 & A2 & A3
-    print("Victory")
 
-elif "B1" and "B2" and "B3" in liste_coup_joueur1:    #B1 & B2 & B3
-    print("Victory")
+def victoire_joueur1():
 
-elif "C1" and "C2" and "C3" in liste_coup_joueur1:    #C1 & C2 & C3
-    print("Victory")
-    
-elif "A1" and "B1" and "C1" in liste_coup_joueur1:    #A1 & B1 & C1
-    print("Victory")
+    if "A1" and "A2" and "A3" in liste_coup_joueur1:      #A1 & A2 & A3
+        print("Victory")
 
-elif "A2" and "B2" and "C2" in liste_coup_joueur1:    #A2 & B2 & C2
-    print("Victory")
+    elif "B1" and "B2" and "B3" in liste_coup_joueur1:    #B1 & B2 & B3
+        print("Victory")
 
-elif "A3" and "B3" and "C3" in liste_coup_joueur1:    #A3 & B3 & C3
-    print("Victory")
+    elif "C1" and "C2" and "C3" in liste_coup_joueur1:    #C1 & C2 & C3
+        print("Victory")
+        
+    elif "A1" and "B1" and "C1" in liste_coup_joueur1:    #A1 & B1 & C1
+        print("Victory")
 
-elif "A1" and "B2" and "C3" in liste_coup_joueur1:    #A1 & B2 & C3
-    print("Victory")
+    elif "A2" and "B2" and "C2" in liste_coup_joueur1:    #A2 & B2 & C2
+        print("Victory")
 
-elif "A3" and "B2" and "C1" in liste_coup_joueur1:    #A3 & B2 & C1
-    print("Victory")
-else:
-    print("Oups")
+    elif "A3" and "B3" and "C3" in liste_coup_joueur1:    #A3 & B3 & C3
+        print("Victory")
+
+    elif "A1" and "B2" and "C3" in liste_coup_joueur1:    #A1 & B2 & C3
+        print("Victory")
+
+    elif "A3" and "B2" and "C1" in liste_coup_joueur1:    #A3 & B2 & C1
+        print("Victory")
+    else:
+        print("Oups")
+
+
+#Fonction qui vérifie si le joueur 2 a gagné
+def victoire_joueur2():
+
+    if "A1" and "A2" and "A3" in liste_coup_joueur2:      #A1 & A2 & A3
+        print("Victory")
+
+    elif "B1" and "B2" and "B3" in liste_coup_joueur2:    #B1 & B2 & B3
+        print("Victory")
+
+    elif "C1" and "C2" and "C3" in liste_coup_joueur2:    #C1 & C2 & C3
+        print("Victory")
+        
+    elif "A1" and "B1" and "C1" in liste_coup_joueur2:    #A1 & B1 & C1
+        print("Victory")
+
+    elif "A2" and "B2" and "C2" in liste_coup_joueur2:    #A2 & B2 & C2
+        print("Victory")
+
+    elif "A3" and "B3" and "C3" in liste_coup_joueur2:    #A3 & B3 & C3
+        print("Victory")
+
+    elif "A1" and "B2" and "C3" in liste_coup_joueur2:    #A1 & B2 & C3
+        print("Victory")
+
+    elif "A3" and "B2" and "C1" in liste_coup_joueur2:    #A3 & B2 & C1
+        print("Victory")
+    else:
+        print("Oups")
+
+case = ["A1", "B1", "C1", "A2", "B2", "C2", "A3", "B3", "C3"]
+
+liste_coup_disponible = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
+liste_coup_joueur1 = []
+liste_coup_joueur2 = []
+
+test = "A3"
+
+liste_coup_disponible.remove(test)
+liste_coup_joueur1.append(test)
+
+print(liste_coup_disponible)
+print(liste_coup_joueur1)
+
+
+while True:
+
+    while True:
+
+        coup_joueur1 = int(input("(Joueur 1) Quel case voulez jouer ?"))
+
+
+        if coup_joueur1 in liste_coup_disponible:
+            liste_coup_joueur1.append
+            liste_coup_disponible.remove
+
+
+
+    coup_joueur2 = int(input("(Joueur 2) Quel case voulez jouer ?"))
