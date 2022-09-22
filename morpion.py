30e23a38446628f8a257759b969cfeecc82a2067
Alex <alex.ardiot@gmail.com>
2022-09-22 13:50:19 +0200

    dicoprint

--- morpion.py
+++ morpion.py
@@ -74,8 +74,11 @@ dicotouche = {
 }
 
 def coupcase(coup_joueur):
-    coup_joueur = dicotouche[coup_joueur]
-    return coup_joueur
+    if coup_joueur > 0 and coup_joueur < 10:
+        coup_joueur = dicotouche[coup_joueur]
+        return coup_joueur
+    else:
+        return False
 
 
 
@@ -84,15 +87,17 @@ liste_coup_disponible = ["A1", "A2", "A3", "B1", "B2", "B3", "C1", "C2", "C3"]
 liste_coup_joueur1 = []
 liste_coup_joueur2 = []
 
-PA1="1"
-PB1="2"
-PC1="3"
-PA2="4"
-PB2="5"
-PC2="6"
-PA3="7"
-PB3="8"
-PC3="9"
+dicoprint = {
+"PA1":"1",
+"PB1":"2",
+"PC1":"3",
+"PA2":"4",
+"PB2":"5",
+"PC2":"6",
+"PA3":"7",
+"PB3":"8",
+"PC3":"9",
+}
 
 def affichageXouO(cellule) :
     if cellule in liste_coup_joueur1:
@@ -105,32 +110,45 @@ while True:
 
     while True:
 
-        print(" ###################\n","#     #     #     #\n","# ",PA1," # ",PB1," # ",PC1," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",PA2," # ",PB2," # ",PC2," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",PA3," # ",PB3," # ",PC3," #\n","#     #     #     #\n","###################")
+        print(" ##### JOUEUR1 #####\n","#     #     #     #\n","# ",dicoprint["PA1"]," # ",dicoprint["PB1"]," # ",dicoprint["PC1"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA2"]," # ",dicoprint["PB2"]," # ",dicoprint["PC2"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA3"]," # ",dicoprint["PB3"]," # ",dicoprint["PC3"]," #\n","#     #     #     #\n","###################")
 
-        coup_joueur1 = int(input("(Joueur 1) Quel case voulez jouer ?"))
+        coup_joueur1 = int(input("(Joueur 1) Quel case voulez-vous jouer ? "))
+        coup_joueur1_defini = coupcase(coup_joueur1)
 
 
 
-        if coup_joueur1 in liste_coup_disponible:
-            liste_coup_joueur1.append(coup_joueur1)
-            liste_coup_disponible.remove(coup_joueur1)
+        if coup_joueur1_defini in liste_coup_disponible:
+            liste_coup_joueur1.append(coup_joueur1_defini)
+            liste_coup_disponible.remove(coup_joueur1_defini)
+            affichageXouO(coup_joueur1_defini)
             break
 
         else:
-            print("Dommage")
-            
+            print("Réessayez")
+
+    if victoire_joueur1():
+        break
+
     
     while True:
 
-        print(" ###################\n","#     #     #     #\n","# ",PA1," # ",PB1," # ",PC1," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",PA2," # ",PB2," # ",PC2," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",PA3," # ",PB3," # ",PC3," #\n","#     #     #     #\n","###################")
+        print(" ##### JOUEUR2 #####\n","#     #     #     #\n","# ",dicoprint["PA1"]," # ",dicoprint["PB1"]," # ",dicoprint["PC1"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA2"]," # ",dicoprint["PB2"]," # ",dicoprint["PC2"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA3"]," # ",dicoprint["PB3"]," # ",dicoprint["PC3"]," #\n","#     #     #     #\n","###################")
 
-        coup_joueur2 = int(input("(Joueur 1) Quel case voulez jouer ?"))
+        coup_joueur2 = int(input("(Joueur 2) Quel case voulez-vous jouer ? "))
+        coup_joueur2_defini = coupcase(coup_joueur2)
 
 
-        if coup_joueur2 in liste_coup_disponible:
-            liste_coup_joueur2.append(coup_joueur2)
-            liste_coup_disponible.remove(coup_joueur2)
+
+        if coup_joueur2_defini in liste_coup_disponible:
+            liste_coup_joueur2.append(coup_joueur2_defini)
+            liste_coup_disponible.remove(coup_joueur2_defini)
+            affichageXouO(coup_joueur2_defini)
             break
 
         else:
-            print("Dommage")
+            print("Réessayez")
+
+    if victoire_joueur2():
+        break
+
+print("Fin de partie, merci d'avoir joué")
