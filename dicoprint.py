8f09170ffb9587558fd03488893a2b6d2e8f1132
Ambroise <contact@ambroise.xyz>
2022-09-22 13:48:29 +0200

    ajout du dicoprint

---
+++ dicoprint.py
@@ -0,0 +1,19 @@
+dicoprint = {
+"PA1":"1",
+"PB1":"2",
+"PC1":"3",
+"PA2":"4",
+"PB2":"5",
+"PC2":"6",
+"PA3":"7",
+"PB3":"8",
+"PC3":"9",
+}
+
+
+
+        print(" ##### JOUEUR1 #####\n","#     #     #     #\n","# ",dicoprint["PA1"]," # ",dicoprint["PB1"]," # ",dicoprint["PC1"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA2"]," # ",dicoprint["PB2"]," # ",dicoprint["PC2"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA3"]," # ",dicoprint["PB3"]," # ",dicoprint["PC3"]," #\n","#     #     #     #\n","###################")
+
+
+
+        print(" ##### JOUEUR2 #####\n","#     #     #     #\n","# ",dicoprint["PA1"]," # ",dicoprint["PB1"]," # ",dicoprint["PC1"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA2"]," # ",dicoprint["PB2"]," # ",dicoprint["PC2"]," #\n","#     #     #     #\n","###################\n","#     #     #     #\n","# ",dicoprint["PA3"]," # ",dicoprint["PB3"]," # ",dicoprint["PC3"]," #\n","#     #     #     #\n","###################")
